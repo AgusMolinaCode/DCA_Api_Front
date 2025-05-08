7859312afaa8400c25ab84c6775e755115fb773e
Agustin <am.motos@hotmail.com>
2025-05-08 00:12:28 -0300

    feat: agrega gráfico de línea en el panel de control con datos de visitas mensuales

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,16 +4,20 @@ import { DashboardContentLogin } from "@/components/dashboard/DashboardContentLo
 import DashboardHoldings from "@/components/dashboard/holdings/DashboardHoldings";
 import DashboardPerformance from "@/components/dashboard/performance/DashboardPerformance";
 import DashboardMainBalance from "@/components/dashboard/balance/DashboardMainBalance";
+import DashboardLineChart from "@/components/dashboard/line-chart/DashboardLineChart";
 
 export default function DashboardPage() {
   return (
     <div className="max-w-[100rem] mx-auto p-2 md:p-8">
       <DashboardContentLogin />
-      <div className="sm:flex justify-start gap-2 items-center">
+      <div className="sm:flex justify-start gap-2 items-center h-[32rem]">
         <DashboardHoldings />
         <div className="flex flex-col items-center gap-2 py-2">
           <DashboardMainBalance />
           <DashboardPerformance />
+        </div>
+        <div className="py-2 h-full w-full">
+          <DashboardLineChart />
         </div>
       </div>
       <div className="space-y-4">
